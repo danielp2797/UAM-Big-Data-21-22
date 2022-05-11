b73c7a96c4136328c167429af1e06b4e73bb770e
Daniel <daniel.perez.efremova@gmail.com>
2022-05-11 23:37:00 +0200

    actualiza practica ciclo de vida, fuentes y seguridad

--- ciclo_vida/Kibana_ElasticSearch/proc.py
+++ ciclo_vida/Kibana_ElasticSearch/proc.py
@@ -11,7 +11,7 @@ from pyspark.sql.window import Window
 
 def correlation_matrix(df, corr_columns, method='pearson'): # (pandas.DataFRame, list, str)
 	
-	# Calcula la matriz de correlaciones usando metodos de Spark. Devuelve un dataframe de pandas con las matriz de correlaciones.
+	# Calcula la matriz de correlaciones usando una app de Spark. Devuelve un dataframe de pandas con las matriz de correlaciones.
     vector_col = "corr_features"
     assembler = VectorAssembler(inputCols=corr_columns, outputCol=vector_col)
     df_vector = assembler.transform(df).select(vector_col)
@@ -33,18 +33,10 @@ if __name__ == "__main__":
 
 	sc = spark.sparkContext
 	sc.setLogLevel("OFF")
-	
 
 	data = spark.read.parquet('hdfs://localhost:9000/bitcoin/input/BTC-USD-analysis.parquet')
-	# adelantamiento de valores
-	w = Window().partitionBy().orderBy(col("Date"))
-	data = data.select("*", lag(f"RSI_lag0").over(w).alias(f"RSI_lag1")).na.drop()
-	data = data.select("*", lag(f"RSI_lag1").over(w).alias(f"RSI_lag2")).na.drop()
-	data = data.select("*", lag(f"PSY_lag0").over(w).alias(f"PSY_lag1")).na.drop()
-	data = data.select("*", lag(f"PSY_lag1").over(w).alias(f"PSY_lag2")).na.drop()
-
-		
-	corr_cols = ['PriceVar', 'Volume', 'PSY_lag0', 'RSI_lag0', 'PSY_lag1', 'RSI_lag1', 'PSY_lag2', 'RSI_lag2']
+	
+	corr_cols = ['PSY', 'RSI','PriceVar', 'Volume', 'VolumeVar', 'volatility']
 	corr_matrix = correlation_matrix(data, corr_cols)
 	print('\n Correlation matrix between indexes and stock prices...')
 	print(corr_matrix)
